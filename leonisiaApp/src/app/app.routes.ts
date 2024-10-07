8691a14d3472457110243f9f27df504bd66d8124
Gersuer Henrique <gersuerhenrique@hotmail.com>
2024-10-07 17:48:49 -0300

    erro

--- leonisiaApp/src/app/app.routes.ts
+++ leonisiaApp/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '/leonisiaApp/src/app/pages/loginscreen/loginscreen.component.html'
+    }
+];
