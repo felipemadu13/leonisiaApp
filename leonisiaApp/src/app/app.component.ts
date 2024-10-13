bd32b28175e09c0b4f16216621211fc99258a785
Gersuer Henrique <gersuerhenrique@hotmail.com>
2024-10-13 00:38:20 -0300

    feat: configuraçoes de rotas

--- leonisiaApp/src/app/app.component.ts
+++ leonisiaApp/src/app/app.component.ts
@@ -1,18 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./pages/home/home/home.component";
 import { SiderbarMenuComponent } from "./pages/home/sidebar-menu/siderbar-menu/siderbar-menu.component";
 import { ResetPasswordComponent } from "./pages/reset-password/reset-password/reset-password.component";
 import { LoginscreenComponent } from './pages/loginscreen/loginscreen.component';
 import { CadastroscreenComponent } from './pages/cadastroscreen/cadastroscreen.component';
+import { FormsModule, NgModel } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, SiderbarMenuComponent, ResetPasswordComponent, LoginscreenComponent, CadastroscreenComponent],
+  imports: [RouterOutlet, HomeComponent, SiderbarMenuComponent, ResetPasswordComponent, LoginscreenComponent, CadastroscreenComponent, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'leonisiaApp';
 }
--- leonisiaApp/src/app/pages/loginscreen/loginscreen.component.ts
+++ leonisiaApp/src/app/pages/loginscreen/loginscreen.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-loginscreen',
   standalone: true,
@@ -6,6 +8,21 @@ import { Component } from '@angular/core';
   templateUrl: './loginscreen.component.html',
   styleUrl: './loginscreen.component.css'
 })
-export class LoginscreenComponent {
 
+
+export class LoginscreenComponent {
+  email: string = '';
+  password: string = '';
+
+  constructor(private router: Router) { }
+  alerta() {
+    this.alerta = () => {
+      alert("clicou")
+    }
+  }
+  goToHome() {
+    this.goToHome = () => {
+      this.router.navigate(['/home'])
+    }
+  }
 }
