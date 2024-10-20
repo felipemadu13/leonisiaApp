7ec4ff46733d59c185fcedd43e70682c44335986
felipemadu13 <felipemadureira.a@hotmail.com>
2024-10-20 13:14:27 -0300

    fix: imagens quebradas

--- leonisiaApp/src/app/pages/config/config.component.ts
+++ leonisiaApp/src/app/pages/config/config.component.ts
@@ -3,12 +3,13 @@ import { SiderbarMenuComponent } from "../home/sidebar-menu/siderbar-menu/siderb
 import { FormsModule } from '@angular/forms';  // Import FormsModule aqui!
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPencil } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-config',
   standalone: true,
-  imports: [SiderbarMenuComponent, CommonModule,
-    FormsModule], 
+  imports: [SiderbarMenuComponent, CommonModule,FormsModule, FontAwesomeModule], 
   providers:[],
   templateUrl: './config.component.html',
   styleUrls: ['./config.component.css']  // Corrigir 'styleUrl' para 'styleUrls'
@@ -16,6 +17,8 @@ import { CommonModule } from '@angular/common';
 export class ConfigComponent {
   email: string = '';
   senha: string = '';
+
+  faPencil = faPencil;
 
   editarCampo(campo: string) {
     if (campo === 'email') {
