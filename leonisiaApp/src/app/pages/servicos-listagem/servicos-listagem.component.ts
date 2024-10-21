143a1486a1210b2e3366b1841895f5e495b3d672
Hagliberto <hagliberto@gmail.com>
2024-10-21 01:42:45 -0300

    Correção da página de listagem de serviços

--- leonisiaApp/src/app/pages/servicos-listagem/servicos-listagem.component.ts
+++ leonisiaApp/src/app/pages/servicos-listagem/servicos-listagem.component.ts
@@ -1,11 +1,11 @@
+// servicos-listagem.component.ts
+
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { CommonModule, CurrencyPipe } from '@angular/common';
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt'; 
-import { LOCALE_ID } from '@angular/core';
-import { NgFor } from '@angular/common';
+import { ServicoService } from '@services/servico.service';
 
 registerLocaleData(localePt); 
 
@@ -20,20 +20,19 @@ registerLocaleData(localePt);
     { provide: 'LOCALE_ID', useValue: 'pt-BR' },
   ]
 })
-
 export class ServicosListagemComponent implements OnInit {
   servicos: any[] = [];
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private servicoService: ServicoService, private router: Router) { }
 
   ngOnInit(): void {
-    this.http.get<any[]>('http://localhost:8000/api/servicos').subscribe(data => {
+    this.servicoService.getServicos().subscribe(data => {
       this.servicos = data;
     });
   }
 
   deletarServico(id: number): void {
-    this.http.delete(`http://localhost:8000/api/servicos/${id}`).subscribe(() => {
+    this.servicoService.deleteServico(id).subscribe(() => {
       this.servicos = this.servicos.filter(servico => servico.id !== id);
     });
   }
