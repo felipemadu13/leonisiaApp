e26e03113defeb138d428207ced1948b9aafcc64
Hagliberto <hagliberto@gmail.com>
2024-10-21 01:40:50 -0300

    adição do modelo servico

--- leonisiaApi/leonisia/models.py
+++ leonisiaApi/leonisia/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Servico(models.Model):
+    nome = models.CharField(max_length=255)
+    descricao = models.TextField(blank=True)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.nome
