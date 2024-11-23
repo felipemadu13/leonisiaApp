e5a14bb5e0d97952c9a2a21790624a64c73678f2
Hagliberto <hagliberto@gmail.com>
2024-11-22 21:35:07 -0300

    Erro após o merge

--- src/app/services/servico-realizado.service.ts
+++ src/app/services/servico-realizado.service.ts
@@ -1,44 +1,7 @@
-// import { Injectable } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-// import { Observable } from 'rxjs';
-// import { ServicoRealizado2 } from '../models/ServicoRealizado2';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class ServicoRealizadoService {
-//   private apiUrl = 'http://localhost:8000/api/servicosrealizados';
-  
-//   constructor(private http: HttpClient) {}
-
-//   getServicos(): Observable<ServicoRealizado2[]> {
-//     return this.http.get<ServicoRealizado2[]>(this.apiUrl);
-//   }
-
-//   getServicoById(id: number): Observable<ServicoRealizado2> {
-//     return this.http.get<ServicoRealizado2>(`${this.apiUrl}/${id}`);
-//   }
-
-//   createServico(servico: ServicoRealizado2): Observable<ServicoRealizado2> {
-//     return this.http.post<ServicoRealizado2>(this.apiUrl, servico);
-//   }
-
-//   updateServico(id: number, servico: ServicoRealizado2): Observable<ServicoRealizado2> {
-//     return this.http.put<ServicoRealizado2>(`${this.apiUrl}/${id}`, servico);
-//   }
-
-//   deleteServico(id: number): Observable<void> {
-//     return this.http.delete<void>(`${this.apiUrl}/${id}`);
-//   }
-// }
-
-
-
-
-
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { ServicoRealizado2 } from '../models/ServicoRealizado2';
 import { AuthService } from './auth.service';
 
@@ -50,32 +13,36 @@ export class ServicoRealizadoService {
 
   constructor(private http: HttpClient, private authService: AuthService) {}
 
-  getServicos(): Observable<ServicoRealizado2[]> {
-    const headers = this.authService.getHeaders();
-    return this.http.get<ServicoRealizado2[]>(this.apiUrl, { headers });
+  private getAuthHeaders() {
+    return { headers: this.authService.getHeaders() };
   }
 
- getServicosComTransacoes(): Observable<any> {
-    return this.http.get<any>(this.apiUrl + '/servicos-realizados-transacoes/');
+  getServicos(): Observable<ServicoRealizado2[]> {
+    return this.http.get<ServicoRealizado2[]>(this.apiUrl, this.getAuthHeaders()).pipe(
+      catchError((error) => {
+        console.error('Erro ao buscar serviços', error);
+        return throwError(() => new Error('Erro ao buscar serviços'));
+      })
+    );
+  }
+
+  getServicosComTransacoes(): Observable<any> {
+    return this.http.get<any>(this.apiUrl + 'servicos-realizados-transacoes/', this.getAuthHeaders());
   }
 
   getServicoById(id: number): Observable<ServicoRealizado2> {
-    const headers = this.authService.getHeaders();
-    return this.http.get<ServicoRealizado2>(`${this.apiUrl}/${id}`, { headers });
+    return this.http.get<ServicoRealizado2>(`${this.apiUrl}${id}/`, this.getAuthHeaders());
   }
 
   createServico(servico: ServicoRealizado2): Observable<ServicoRealizado2> {
-    const headers = this.authService.getHeaders();
-    return this.http.post<ServicoRealizado2>(this.apiUrl, servico, { headers });
+    return this.http.post<ServicoRealizado2>(this.apiUrl, servico, this.getAuthHeaders());
   }
 
   updateServico(id: number, servico: ServicoRealizado2): Observable<ServicoRealizado2> {
-    const headers = this.authService.getHeaders();
-    return this.http.put<ServicoRealizado2>(`${this.apiUrl}/${id}`, servico, { headers });
+    return this.http.put<ServicoRealizado2>(`${this.apiUrl}${id}/`, servico, this.getAuthHeaders());
   }
 
   deleteServico(id: number): Observable<void> {
-    const headers = this.authService.getHeaders();
-    return this.http.delete<void>(`${this.apiUrl}/${id}`, { headers });
+    return this.http.delete<void>(`${this.apiUrl}${id}/`, this.getAuthHeaders());
   }
 }
